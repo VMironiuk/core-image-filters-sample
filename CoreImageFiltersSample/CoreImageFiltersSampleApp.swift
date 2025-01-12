5686ec9ffbd6399c4b8846478b72e22505003fa2
Vova Myroniuk <vladimironiuk@gmail.com>
2025-01-12 14:55:41 +0200

    Add starter project

---
+++ CoreImageFiltersSample/CoreImageFiltersSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageFiltersSampleApp.swift
+//  CoreImageFiltersSample
+//
+//  Created by Vova Myroniuk on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageFiltersSampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
